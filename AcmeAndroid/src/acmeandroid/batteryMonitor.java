5f8d8eabd2be60ab9cdf845b42811cf1c02e036e
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-05-13 18:16:33 +0100

    Update batteryMonitor.java

--- AcmeAndroid/src/acmeandroid/batteryMonitor.java
+++ AcmeAndroid/src/acmeandroid/batteryMonitor.java
@@ -17,4 +17,26 @@ package acmeandroid;
  */
 public class batteryMonitor {
     
+    private double energyConsumption;
+    private final int batteryMinLevel = 1;
+    private final int batteryMaxLevel = 8;
+    private double batteryActualLevel;
+    private long miliseconds;
+    
+    
+    /*
+        the power plant is a cheap model of lithium battery from AndroidsRus and 
+        can only produce 8 volts, its recovery time after discharging is 3 seconds.(2.6 volts per second)
+    */
+    public void battaryCheck(double energyConsumption){
+        this.energyConsumption = energyConsumption;
+        
+        // battery check recharge
+        if (batteryActualLevel == batteryMinLevel){
+            
+            Thread.sleep(miliseconds);
+        }
+    
+    }
+    
 }

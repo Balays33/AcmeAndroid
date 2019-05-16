baa413b347518b4a830dee9b92909529d71b16d4
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-05-16 18:50:00 +0100

    back

---
+++ AcmeAndroid/src/acmeandroid/MotorJoint.java
@@ -0,0 +1,89 @@
+package acmeandroid;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jhoms Mosquera
+ * @author Eduardo Nakashima
+ * @author Balazs Barcza
+ * @author Javier Lopez Santacruz Serraller
+ * @author Michał Świtała
+ * 
+ */
+public class MotorJoint {
+    
+<<<<<<< HEAD
+
+    
+
+=======
+>>>>>>> 58653e8843064a8f1a35dd39ecd604e1365704a8
+    private final double maximumRotation;
+    private final double maximumFlexion;
+    private final double energyConsumption;
+    private double currentRotation;
+    private double currentFlexion;
+<<<<<<< HEAD
+    private final double energyConsumption;
+    private double motorCurrentPosition;
+    private double motorCurrentFlexion; // gettter and setter
+
+=======
+    // gettter and setter
+>>>>>>> 58653e8843064a8f1a35dd39ecd604e1365704a8
+
+    public MotorJoint(double maximumRotation, double maximumFlexion, double energyConsumption) {
+        this.maximumRotation = maximumRotation;
+        this.maximumFlexion = maximumFlexion;
+        this.energyConsumption = energyConsumption;
+    }
+
+<<<<<<< HEAD
+    
+=======
+    public double getMaximumRotation() {
+        return maximumRotation;
+    }
+
+    public double getCurrentRotation() {
+        return currentRotation;
+    }
+
+    public double getMaximumFlexion() {
+        return maximumFlexion;
+    }
+
+    public double getCurrentFlexion() {
+        return currentFlexion;
+    }
+
+    public double getEnergyConsumption() {
+        return energyConsumption;
+    }
+
+    public void setCurrentRotation(double currentRotation) {
+        this.currentRotation = currentRotation;
+    }
+
+    public void setCurrentFlexion(double currentFlexion) {
+        this.currentFlexion = currentFlexion;
+    }
+>>>>>>> 58653e8843064a8f1a35dd39ecd604e1365704a8
+
+    @Override
+    public String toString() {
+        // has to be setup to print out the object value
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+    
+    
+    
+}
---
+++ AcmeAndroid/src/acmeandroid/Setup.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acmeandroid;
+
+/**
+ *
+ * @author Jhoms Mosquera
+ * @author Eduardo Nakashima
+ * @author Balazs Barcza
+ * @author Javier Lopez Santacruz Serraller
+ * @author Michał Świtała
+ * 
+ */
+public class Setup 
+{
+    public Setup() {}
+    
+<<<<<<< HEAD
+    MotorJoint Ankle;
+    MotorJoint Knee;
+    MotorJoint Hip;
+    MotorJoint Waist;
+    MotorJoint  Wrist;
+    MotorJoint Elbow;
+    MotorJoint Shoulder;
+    MotorJoint Neck;
+    MotorJoint Head;
+    
+    Data d = new Data();
+    
+    private void setupMethod(){
+       // Ankle =  new MotorJoint(d.getAnkleRotation(), 0, 0, 0, d.getAnkleVoltage()); // this is start should update it
+       Ankle = new MotorJoint(0, 0, 0, 0, 0);
+    }
+    
+    private void printOutData(MotorJoint passObject){
+        passObject.toString();
+=======
+    public MotorJoint[] setupMethod()
+    {
+        Data d = new Data();
+        MotorJoint[] m = new MotorJoint[d.getMotorName().length];
+        //loop to generate the motors in an array
+        for(int i=0;i<d.getMotorName().length;i++)
+        {
+            //        MotorJoint(double maximumRotation, double maximumFlexion, double energyConsumption) {
+            m[i]= new MotorJoint(d.getMaxRotation()[i],d.getMaxFlextion()[i],d.getEnergyConsumption()[i]);
+        }
+        return m;
+>>>>>>> 58653e8843064a8f1a35dd39ecd604e1365704a8
+    }
+
+}
+    

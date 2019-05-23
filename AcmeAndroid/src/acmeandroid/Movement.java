04650539891539f64e977286d4eeea63ba5365f1
eduardokzn <eduardo.k.z.n@gmail.com>
2019-05-24 00:08:55 +0100

    Structure reviewed

--- AcmeAndroid/src/acmeandroid/Movement.java
+++ AcmeAndroid/src/acmeandroid/Movement.java
@@ -5,6 +5,8 @@
  */
 package acmeandroid;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jhoms Mosquera
@@ -14,29 +16,59 @@ package acmeandroid;
  * @author Michał Świtała
  * 
  */
+
 public class Movement 
 {
-
+    private DataMovement dm;
+    private DataSit ds;
+    private DataStand du;
+    private MotorJoint[] mj;
+    private SetupStartPosition ssp;
+    private batteryMonitor bm;
     public Movement() 
     {
-        
     }
     
     public void start(int input)
     {
         switch (input)
         {
-        case 1:
+        case 1://stand up
+            moving(ds,dm.getStandUp());
             break;
-        case 2:
+        case 2://walk
+            moving(du,dm.getWalkForward());
             break;
-        case 3:
+        case 3://sit down
+            moving(du,dm.getSitDown());
             break;
         default:
             break;
             
         }
         
+    }
+
+    private void moving(DataSit d,ArrayList ar ) 
+    {   
+        MotorJoint mm;
+        int[] move;
+        ssp.setupMethod(d.getRotation(), d.getFlextion()); //set the initial position
+        for(int i=0;i<ar.size();i++)
+        {
+            move = (int[]) ar.get(i);
+            bm.allowMove(move[0], move[1], (double)move[2]);
+            //set motor to the required position
+            mm = mj[move[0]];
+            if(move[1]==1)
+            {
+                mm.setCurrentFlexion((double)move[2]);
+            }
+            else
+            {
+                mm.setCurrentRotation((double)move[2]);
+            }
+        }
     }
     
 

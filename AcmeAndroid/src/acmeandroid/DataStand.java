6b691c958b7ced41e9990a959cff3520485bd67d
eduardokzn <eduardo.k.z.n@gmail.com>
2019-05-23 23:39:11 +0100

    Structure reviewed

--- AcmeAndroid/src/acmeandroid/DataStand.java
+++ AcmeAndroid/src/acmeandroid/DataStand.java
@@ -14,80 +14,59 @@ package acmeandroid;
  * @author Michał Świtała
  * 
  */
-public class DataStand {
-    
-     /*
-        This variable should be set up the right position to stand 
-    */
+public class DataStand extends DataSit
+{
 
     /*
-        This variable should be set up the right position to sit
-    */
-    
-    
-    
-    private final double[] rotationIni = new double[]
+    This variable should be set up the right position to stand
+     */
+    private final double[] rotationIni ;    
+    private final double[] flextionIni ;    
+
+    public DataStand() 
     {
-        0,//                "Head",//0  - All motors is inital at 0 when standing 
-        0,//                "Neck",//1    exept the ancle in 90 degress. 
-        0,//                "Shoulder-Right",
-        0,//                "Shoulder-Left",
-        0,//                "Elbow-Right",
-        0,//                "Elbow-Left",
-        0,//                "Wrist-Right",
-        0,//                "Wrist-Left",
-        0,//                "Waist",
-        0,//                "Hip-Right",
-        0,//                "Hip-Left",
-        0,//                "Knee-Right",
-        0,//                "Knee-Left",
-        90,//                "Ankle-Right",
-        90//                 "Ankle-Left"//14
-    };    
+        rotationIni = new double[]
+        {
+            0,//                "Head",//0  - All motors is inital at 0 when standing 
+            0,//                "Neck",//1    exept the ancle in 90 degress. 
+            0,//                "Shoulder-Right",
+            0,//                "Shoulder-Left",
+            0,//                "Elbow-Right",
+            0,//                "Elbow-Left",
+            0,//                "Wrist-Right",
+            0,//                "Wrist-Left",
+            0,//                "Waist",
+            0,//                "Hip-Right",
+            0,//                "Hip-Left",
+            0,//                "Knee-Right",
+            0,//                "Knee-Left",
+            90,//                "Ankle-Right",
+            90//                 "Ankle-Left"//14
+        }; 
+        flextionIni = new double[]
+        {
+            0,//                "Head",//0 - All initial position of the standing position is 
+            0,//                "Neck",//1   initialise with 0.  
+            0,//                "Shoulder-Right",
+            0,//                "Shoulder-Left",
+            0,//                "Elbow-Right",
+            0,//                "Elbow-Left",
+            0,//                "Wrist-Right",
+            0,//                "Wrist-Left",
+            0,//                "Waist",
+            0,//                "Hip-Right",
+            0,//                "Hip-Left",
+            0,//                "Knee-Right",
+            0,//                "Knee-Left",
+            0,//                "Ankle-Right",
+            0//                 "Ankle-Left"//14
+        };
+    }
+
+
     
     
-    private final double[] flextionIni = new double[]
-    {
-        0,//                "Head",//0 - All initial position of the standing position is 
-        0,//                "Neck",//1   initialise with 0.  
-        0,//                "Shoulder-Right",
-        0,//                "Shoulder-Left",
-        0,//                "Elbow-Right",
-        0,//                "Elbow-Left",
-        0,//                "Wrist-Right",
-        0,//                "Wrist-Left",
-        0,//                "Waist",
-        0,//                "Hip-Right",
-        0,//                "Hip-Left",
-        0,//                "Knee-Right",
-        0,//                "Knee-Left",
-        0,//                "Ankle-Right",
-        0//                 "Ankle-Left"//14
-    };    
+    
 
    
-    public double [] getRotation() {
-        
-        return rotationIni;
-    }
-    
-  
-    public double [] getFlextion() {
-        
-        return flextionIni;
-    }
-    
-     //Return one rotation only at specified index
-    public double getRotationAt(int index) {
-        
-        return rotationIni [index];
-    }
-    
-  //Return one flextion only at specified index
-    public double getFlextionAt(int index) {
-        
-        return flextionIni [index];
-    }
-        
-        
 }

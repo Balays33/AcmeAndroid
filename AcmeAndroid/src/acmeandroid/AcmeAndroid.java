003fc34aa45dcdeb717757e618d4f590fcc23daf
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-05-26 20:36:58 +0100

    baterry 90%

--- AcmeAndroid/src/acmeandroid/AcmeAndroid.java
+++ AcmeAndroid/src/acmeandroid/AcmeAndroid.java
@@ -1,100 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package acmeandroid;
 
-/**
- *
- * @author Jhoms Mosquera
- * @author Eduardo Nakashima
- * @author Balazs Barcza
- * @author Javier Lopez Santacruz Serraller
- * @author Michał Świtała
- * 
- */
-public class AcmeAndroid {
-
-    
-    
-    
-    public static void main(String[] args) {
-        /*
-        Logic logic1 = new Logic();
-        Printer printer1 = new Printer();
-        
-        
-        
-        printer1 = logic1.printWelcome(printer1);
-        logic1.showMenu();
-        */
-        // comment by Balazs 
-            // test the batterymonitor  Knee  : 90 deg.  Voltage required : 3 volts
-        Printer printer = new Printer();    
-        batteryMonitor bm = new batteryMonitor(printer);
-        bm.setBatteryCurrentLevel(2);
-        bm.allowMove(1, 0,78);
-       
-        
-        
-        /*
-        // comment by Michal
-        MotorJoint[] test2;
-        Setup test = new Setup();
-        
-        // comment by Balazs 
-            // test the batterymonitor  Knee  : 90 deg.  Voltage required : 3 volts
-        batteryMonitor bm = new batteryMonitor();
-        bm.setBatteryCurrentLevel(2);
-        bm.allowMove(1, 0, 75);
-
-        // only for testing
-        // comment by Michal
-
-<<<<<<< HEAD
-        //System.out.println("Creating array of MotorJoin objects");
-        //test2 = test.setupMethod(MotorJoint);
-
-        //System.out.println("Creating array of MotorJoin objects");        
-        //test2 = test.setupMethod();        
-=======
-        System.out.println("Creating array of MotorJoin objects");
-  //test2 = test.setupMethod(MotorJoint);
-
-        System.out.println("Creating array of MotorJoin objects");        
-        test2 = test.setupMethod();        
->>>>>>> ad2f0cb14802328590f556d2fc705d8a4d250244
-
-        System.out.println("MotorJoin objects created. ");
-
-        // this is just test for loop - delete it later
-        // comment by Michal
-<<<<<<< HEAD
-        /*
-        for (int i = 0; i < test2.length; i++) {
-=======
-        for (int i = 0; i < test2.length; i++)
-        {
->>>>>>> ad2f0cb14802328590f556d2fc705d8a4d250244
-            // checking data
-            System.out.println("Energy consumption in MotorJoint number : " + i + " is : " + test2[i].getEnergyConsumption());
-            System.out.println("Max flexion in MotorJoint number : " + i + " is : " + test2[i].getMaximumFlexion());
-
-        }
-<<<<<<< HEAD
-        */
-
-
-        
-
-    }
-    
-    
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -123,6 +27,11 @@ public class AcmeAndroid {
         //flow
         p.welcome();
         mn.showMenu();
+        
+        Printer printer = new Printer();    
+        batteryMonitor bm = new batteryMonitor(printer);
+        bm.setBatteryCurrentLevel(2);
+        bm.allowMove(1, 0,78);
         /*
         
         // comment by Michal
@@ -152,4 +61,4 @@ public class AcmeAndroid {
     
     
 }
->>>>>>> 7f45d0935b31e3f02e263cf1c372b60f89d32f7f
+
--- AcmeAndroid/src/acmeandroid/Printer.java
+++ AcmeAndroid/src/acmeandroid/Printer.java
@@ -1,132 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package acmeandroid;
-
-/**
- *
- * @author Jhoms Mosquera
- * @author Eduardo Nakashima
- * @author Balazs Barcza
- * @author Javier Lopez Santacruz Serraller
- * @author Michał Świtała
- *
- */
-public class Printer {
-
-    
-   // Fields of data
-       
-   private String[] motorNames;
-    
-    
-
-    public void setMotorNames(String[] motoNames) {
-
-        this.motorNames = motoNames;
-    }
-
-    /**
-     * @param passObject MotorJoint object needs to be passed inside
-     */
-    public void printOutData(MotorJoint passObject) {
-
-        passObject.toString();
-    }
-
-    /**
-     * Prints out welcome message to the user.
-     */
-    public void welcome() {
-
-        System.out.println("Welcome in AcmeAndroid.");
-        System.out.println(" ");
-        System.out.println("Acme Robotics have developed an Android, in collaboration with their partners AndroidsRus.");
-        System.out.println("They have sourced the motors to be used to articulate the android, along with a cheap power plant.");
-        System.out.println("This application will allow you to choose and make an Android stand, walk and sit down with human-like movement.");
-        System.out.println("You will also be able to see the progress of each pose transitions.");
-        System.out.println(" ");
-
-    }
-
-    /**
-     *
-     * @param motorIndex the index of the motorJoint to be selected
-     * @param current the current position (degree) of the current motorJoint
-     * @param batteryEnergy the current energy of the battery
-     */
-    public void sittingPosition(int motorIndex, double current, double batteryEnergy) {
-
-        System.out.println(" ");
-        System.out.println("The Android is currently sitting on the chair.");
-        System.out.println(" ");
-        System.out.println("The motor " + motorNames[motorIndex] + " is currently at: " + current + " degrees.");
-        System.out.println("The battery has " + batteryEnergy + " energy left.");
-    }
-
-    /**
-     *
-     * @param motorIndex the index of the motorJoint to be selected
-     * @param current the current position (degree) of the current motorJoint
-     * @param batteryEnergy the current energy of the battery
-     */
-    public void standingPosition(int motorIndex, double current, double batteryEnergy) {
-
-        System.out.println(" ");
-        System.out.println("The Android is currently standing.");
-        System.out.println(" ");
-        System.out.println("The motor " + motorNames[motorIndex] + " is currently at: " + current + " degrees.");
-        System.out.println("The battery has " + batteryEnergy + " energy left.");
-
-    }
-
-    /**
-     *
-     * @param motorIndex the index of the motorJoint
-     * @param moveType 0 = rotates, 1 = flextionate
-     * @param current current position of the motorJoint
-     * @param finalPositon final position of the motorJoint
-     * @param energyConsumed energy that has been consumed during movement
-     */
-    public void printMove(int motorIndex, int moveType, double current, double finalPositon, double energyConsumed) {
-
-        String mt = " rotates ";
-        if (moveType == 1) {
-            mt = " flextionate ";
-        }
-
-        System.out.println(" ");
-        System.out.println("The Android is currently moving.");
-        System.out.println(" ");
-        System.out.println("The motor " + motorNames[motorIndex] + mt + "from " + current + " to " + finalPositon + " degrees.");
-        System.out.println("The battery has been consumed by " + energyConsumed + " Volts. ");
-
-    }
-
-    public void batteryMonitorInfo(double sixtydegree,double jointmove){
-        // print out  if movement is more than 60% of available motion for a motor
-        System.out.println("sixtydegree = " + sixtydegree);
-        System.out.println("join move :" + jointmove);
-    }
-    
-    public void printMovmentTake(long sleepingMiliSecondsMovement){
-        System.out.println("The join is moving is take : " +sleepingMiliSecondsMovement + " milliseconds");
-    }
-    
-    public void printEnergyConsumption(double energyConsumption){
-        System.out.println("EnergyConsumption level: " + energyConsumption);
-    }
-    
-    public void printBatteryLevel(double batteryCurrentLevel){
-        System.out.println("Battery level : " + batteryCurrentLevel);
-    }
-    
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -241,7 +112,25 @@ public class Printer {
 
     }
 
+    public void batteryMonitorInfo(double sixtydegree,double jointmove){
+        // print out  if movement is more than 60% of available motion for a motor
+        System.out.println("sixtydegree = " + sixtydegree);
+        System.out.println("join move :" + jointmove);
+    }
+    
+    public void printMovmentTake(long sleepingMiliSecondsMovement){
+        System.out.println("The join is moving is take : " +sleepingMiliSecondsMovement + " milliseconds");
+    }
+    
+    public void printEnergyConsumption(double energyConsumption){
+        System.out.println("EnergyConsumption level: " + energyConsumption);
+    }
+    
+    public void printBatteryLevel(double batteryCurrentLevel){
+        System.out.println("Battery level : " + batteryCurrentLevel);
+    }
+    
 
     
 }
->>>>>>> 7f45d0935b31e3f02e263cf1c372b60f89d32f7f
+
